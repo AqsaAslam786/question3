4d803f42c1b802ee735e6f77629b52e25568d17b
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-06 22:36:28 +0500

    save

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Flag from './components/FlagsComponent'
+
+function App() {
+  
+
+  return (
+    <>
+     <Flag />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FlagsComponent.tsx
@@ -0,0 +1,23 @@
+type FlagProps = {
+    name: string;
+    color1: string;
+    color2: string;
+    color3: string;
+    horizontal: boolean;
+}
+
+const Flag = ({name, color1, color2, color3, horizontal}: FlagProps) => {
+  return (
+    <div className="h-72 w-120">
+        <h1 className="text-2xl font-bold pb-10">{name}</h1>
+      <div className={`flex ${horizontal ? "flex-col" : "flex-row"}
+      w-69 h-60 border border-gray-300`}>
+        <div className={`${horizontal ? "h-1/3 w-full" : "w-1/3 h-full"} ${color1}`}>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Flag
