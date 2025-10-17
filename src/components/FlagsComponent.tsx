0ef3461722eb0079567a82c4d7332803d518492f
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-18 00:01:20 +0500

    save

--- src/components/FlagsComponent.tsx
+++ src/components/FlagsComponent.tsx
@@ -10,8 +10,9 @@ const Flag = ({name, color1, color2, color3, horizontal}: FlagProps) => {
   return (
     <div className="h-72 w-120">
         <h1 className="text-2xl font-bold pb-10">{name}</h1>
-      <div className={`flex ${horizontal ? "flex-col" : "flex-row"}
-      w-69 h-60 border border-gray-300`}>
+      <div className={`flex ${
+        horizontal ? "flex-col" : "flex-row"
+      } w-69 h-60 border border-gray-300`}>
         <div className={`${horizontal ? "h-1/3 w-full" : "w-1/3 h-full"} ${color1}`}>
         </div>
         <div className={`${horizontal ? "h-1/3 w-full" : "w-1/3 h-full"} ${color2}`}></div>
